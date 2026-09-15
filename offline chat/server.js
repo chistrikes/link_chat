c404796d29f698b17aa53d162467d856d903d640
chistrikes <chistrikesxevora@gmail.com>
2026-09-15 22:03:24 +0100

    Add files via upload
    Initial Link Chat upload

---
+++ offline chat/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("chat message", (message) => {
+        console.log("Message:", message);
+
+        io.emit("chat message", message);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Chat server running on port ${PORT}`);
+});
